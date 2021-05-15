f4d3d7cba50b853ce20f28b1a69942128520dbb4
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-15 10:10:34 +0100

    Create section four

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
+  
+  // Panel //
+
+  // Panel boddy display toggle
+  $('.panel .icon').click(function() {
+    $(this).closest('.panel').find('.panel-body').slideToggle();
+
+    if ($(this).hasClass('arrow-up')) {
+      $(this).addClass('arrow-down');
+      $(this).removeClass('arrow-up');
+
+    } else {
+      $(this).addClass('arrow-up');
+      $(this).removeClass('arrow-down');
+
+    }
+
+  });
 
 
-});
+})
