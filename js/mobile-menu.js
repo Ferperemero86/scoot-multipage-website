d879ddbf303c6dcb63f0d07add54bacc8e3e48ee
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-16 09:29:00 +0100

    Create fixed menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
 
   // Menu //
+  const mainMenu = $('.main-menu');
+  const mobileMenu = $('.mobile-menu');
+  const closeIcon =  $('.close-icon');
+  const hamburger = $('.hamburger');
 
   // Show menu
-  $('.hamburger').click(function() {
-    $('.mobile-menu').addClass('slide-right');
-    $('.mobile-menu').removeClass('slide-left');
+  hamburger.click(function() {
+    mobileMenu.addClass('slide-right');
+    mobileMenu.removeClass('slide-left');
     $(this).addClass('hidden');
-    $('.close-icon').removeClass('hidden');
+    closeIcon.removeClass('hidden');
   });
 
 
   // Hide menu
-  $('.close-icon').click(function() {
-    $('.mobile-menu').addClass('slide-left');
-    $('.mobile-menu').removeClass('slide-right');
+  closeIcon.click(function() {
+    mobileMenu.addClass('slide-left');
+    mobileMenu.removeClass('slide-right');
     $(this).addClass('hidden');
-    $('.hamburger').removeClass('hidden');
+    hamburger.removeClass('hidden');
+  });
+
+  // Fixed menu on scroll
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 125) {
+      mainMenu.addClass('fixed');
+
+    } else {
+      mainMenu.removeClass('fixed');
+    }
   });
 
 });
